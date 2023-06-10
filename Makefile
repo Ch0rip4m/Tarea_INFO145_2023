e1487b2817331c165a325a65891ee249f50f5858
Ch0rip4m <giorgio.nicolini.a@gmail.com>
2023-06-10 19:59:01 -0400

    Test C++ pa acordarme como usarlo xd

---
+++ Makefile
@@ -0,0 +1,2 @@
+all:
+	g++ -O3 saltos.cpp -o prog
--- saltos.cpp
+++ saltos.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+using namespace std;
 
+
+int main(int argc, char* argv[]){
+    int n = atoi(argv[1]), i, j;
+    // imprimir caracteres
+    for (i = 0; i < n; i++){
+        for (j = 0; j<i; j++ ){
+            cout << argv[2];
+        }
+        cout << endl;
+    }
+    return 0;
+}
